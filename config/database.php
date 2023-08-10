531653ddb1037317629c969b95e402507fd4b33e
vm84039 <69450472+vm84039@users.noreply.github.com>
2023-08-10 12:17:56 -0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlsrv'),
 
     /*
     |--------------------------------------------------------------------------
@@ -81,11 +81,11 @@ return [
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'tcp:booklibrary.database.windows.net'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'BookLibrary'),
+            'username' => env('DB_USERNAME', 'CloudSAc9403042'),
+            'password' => env('DB_PASSWORD', '8160Prest!'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
